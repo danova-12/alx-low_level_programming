5581975018acbfb1138e39248f7b5479646dc956
dani <danielabesha7@gmail.com>
2022-12-08 08:56:01 -0800

    ertjgg

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *the function prints string
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilinguial puzzle");
+	return (0);
+}
