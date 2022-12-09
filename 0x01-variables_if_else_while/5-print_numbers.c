c4de879e1f5cdfc9f8f0e153d954f18e2aef80c9
dani <danielabesha7@gmail.com>
2022-12-09 04:36:26 -0800

    eufre

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - return single integer from 0-10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int number = 0;
+
+	while (number < 10)
+
+	{
+		printf("%d", number);
+		number++;
+	}
+	putchar('\n')
+	return (0);
+}
+
