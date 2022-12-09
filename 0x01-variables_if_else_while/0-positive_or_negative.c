969168e65ed50da324534a230019a8c3adf73f0a
dani <danielabesha7@gmail.com>
2022-12-09 06:32:52 -0800

    ches

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,16 +14,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%dis positive\n", n);
+		printf("%d is positive", n);
 
 	}
 	else if (n == 0)
 	{
-		printf("%dis zero\n", n);
+		printf("%d is zero", n);
 	}
 	else
 	{
-		printf("%dis negative\n", n);
+		printf("%d is negative", n);
 	}
 	printf("\n");
 	return (0);
