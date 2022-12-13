f3ca9100966f31f4a4665b1fffc9d09da36f1bd4
dani <danielabesha7@gmail.com>
2022-12-13 02:04:25 -0800

    prints lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - function print alphabet
+ *
+ * return : none
+ */
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha < 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
+
