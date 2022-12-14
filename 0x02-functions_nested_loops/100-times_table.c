cf43b822692ec46968c9966ec285629c1fdd4547
dani <danielabesha7@gmail.com>
2022-12-14 11:02:46 -0800

    hgf

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,11 +13,11 @@ void print_times_table(int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		for ( j = 0; j <= n; j++)
+		for (j = 0; j <= n; j++)
 		{
-			if (n > 0 && n < = 15)
+			if (n > 0 && n <= 15)
 			{	
-				_putchar(i * j);
+				_putchar('i * j');
 				_putchar(',');
 				_putchar('  ');	
 			}
