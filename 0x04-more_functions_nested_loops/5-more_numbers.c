10d4374b617209ebc372780c35d513f5e34de5ad
dani <danielabesha7@gmail.com>
2022-12-15 05:41:03 -0800

    cuyrfe

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - print 10 times
+ *
+ * Return: none
+ */
+void more_numbers(void)
+{
+	for (i = 0; i <= 9; i++)
+	{
+		int a = '0';
+
+		while (a <= '14')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+	}
+
