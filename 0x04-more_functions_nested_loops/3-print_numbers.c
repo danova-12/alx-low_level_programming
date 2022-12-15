b80438fc390f05c8d5365ee42dd2cf84b8f84e7c
dani <danielabesha7@gmail.com>
2022-12-15 03:59:04 -0800

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - print digits
+ *
+ * Return: nothing;
+ */
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
