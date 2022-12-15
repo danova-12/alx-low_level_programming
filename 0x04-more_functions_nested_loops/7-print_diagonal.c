6aa8c1e4d284ac1bc7c631294b4ee6e68d0e1fdc
dani <danielabesha7@gmail.com>
2022-12-15 11:12:24 -0800

    dghgeui

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+##include "main.h"
+
+/**
+ * print_diagonal - print slash
+ * @n: params
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
