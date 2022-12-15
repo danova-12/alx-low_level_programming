e6bcc9975ac15a8f25b461d80b0f3d5e11070d86
dani <danielabesha7@gmail.com>
2022-12-15 07:53:07 -0800

    ufrrf

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,14 @@
  */
 void print_line(int n)
 {
-	for (int i = 0; i <= n; i++)
+	if (n >= 0)
 	{
-		if (n > 0)
+		for ( i = 0; i < n; i++)
 			_putchar('_');
-		else
-			_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{	
+		_putchar('\n');
+	}	
+	_putchar('\n');	
 }
