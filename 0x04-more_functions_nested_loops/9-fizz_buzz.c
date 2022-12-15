91bd1d2fa3aa85cc913096e68bdc431f4bd833c0
dani <danielabesha7@gmail.com>
2022-12-15 09:29:47 -0800

    yrruyyuit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5)
+			printf("FizzBuzz");
+		if else (i % 3 == 0)
+		 	printf("Fizz");
+		if else (i % 5 == 0)
+			printf("Buzz");
+		else
+			
+			printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+}	
+
+
+
+
+
+
+
+
