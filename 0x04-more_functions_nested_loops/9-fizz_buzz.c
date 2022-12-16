feeb3c86b2b766881066f3702ac753362a0ea1d9
dani <danielabesha7@gmail.com>
2022-12-16 04:53:45 -0800

    uruigi

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	for (int i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5)
+		if ((i % 3 == 0) && (i % 5))
 			printf("FizzBuzz");
-		if else (i % 3 == 0)
+		if else ((i % 3 == 0))
 		 	printf("Fizz");
-		if else (i % 5 == 0)
+		if else ((i % 5 == 0))
 			printf("Buzz");
 		else
 
