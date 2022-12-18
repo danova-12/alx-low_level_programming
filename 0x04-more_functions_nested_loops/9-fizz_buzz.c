02baf9712e0f2bbd4c6a7d4422422e0c2ad06cc2
dani <danielabesha7@gmail.com>
2022-12-18 01:30:04 -0800

    ifirif

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i = 1;
 
-	for (; i <= 100; i++)
+	for (; i <= 99; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz");
@@ -19,7 +19,7 @@ int main(void)
 		else
 
 			printf("%d", i);
-		printf("");
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
