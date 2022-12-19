4d84516b5acfe682f3f4597bad1653b954fed676
dani <danielabesha7@gmail.com>
2022-12-19 13:08:15 -0800

    fgr

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - Function prints element of array of integers
+ * @a: Pointer parameter
+ * @n: integer variable
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i])
+		{
+			if (i != (n - 1))
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+}
