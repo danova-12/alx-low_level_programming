2d9543c9b79f96314be313aa8d474868347b5c49
dani <danielabesha7@gmail.com>
2023-01-02 07:16:55 -0800

    jkfurf

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory data
+ * @dest: dest address
+ * @src: source
+ * @n: size
+ * Return: pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (; i < n; i++)
+	{
+		dest[i] = dest[i];
+	}
+	return (dest);
+}
