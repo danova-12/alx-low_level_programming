e1e5ebe2d45f9f7661bc477446d8b476de7ba4c4
dani <danielabesha7@gmail.com>
2023-01-02 10:25:59 -0800

    rfrugyf

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#define NULL 0
+
+/**
+* _strpbrk - return pointer
+* @s: string to search
+* @accept: target matches
+* Return: pointe
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int x = 0, y;
+
+	while (s[x] != '\0')
+	{
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (s[x] == accept[y])
+			{
+				s = &s[x];
+				return (s);
+			}
+
+		}
+
+		x++;
+	}
+
+	return (NULL);
+
+}
