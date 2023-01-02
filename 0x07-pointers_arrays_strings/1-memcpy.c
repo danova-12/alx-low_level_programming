ee6db49445920a5e6e6117dc467c118249bc6956
dani <danielabesha7@gmail.com>
2023-01-02 07:39:47 -0800

    fuhi5frcdo

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,11 +8,15 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	int size = n;
 
-	for (i = 0; i < n; i++)
+	if (size > 0)
 	{
-		dest[i] = dest[i];
+		int i;
+
+		for (i = 0; i < size; i++)
+		dest[i] = src[i];
 	}
+
 	return (dest);
 }
