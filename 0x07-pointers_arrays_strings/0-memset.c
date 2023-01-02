e8273e3eb2ca2cb84fd63f02530a8ff23acdfde0
dani <danielabesha7@gmail.com>
2023-01-02 06:56:09 -0800

    dgug

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - copy from one ml to ml
+ * @s: pointer
+ * @b: data
+ * @n: size
+ *
+ * Return: ponter
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = o;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++
+	}
+	return (s)
+}
