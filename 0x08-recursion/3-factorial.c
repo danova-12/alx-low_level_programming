21c5c521dabc6e4d106b2b3d8039a6f9de551de3
dani <danielabesha7@gmail.com>
2023-01-04 04:07:01 -0800

    task 4

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * factorial - compute factorial
+ * @s: pointer
+ *
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n == 1)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
