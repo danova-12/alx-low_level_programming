08f6eb4d3c96d1fc710078b79edb7b9ddceeeabf
dani <danielabesha7@gmail.com>
2023-01-04 04:11:44 -0800

    task3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,8 +7,12 @@
  */
 int factorial(int n)
 {
-	if (n == 1)
-		return (1);
-	else
-		return (n * factorial(n - 1));
+	if ( n > 0)
+	{	
+		if (n == 1)
+			return (1);
+		else
+			return (n * factorial(n - 1));
+	
+	}
 }
