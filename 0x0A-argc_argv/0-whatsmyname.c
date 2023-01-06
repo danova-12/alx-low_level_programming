b971c56973ce8f5fc6ecee8c21da1cfe6160dfd0
dani <danielabesha7@gmail.com>
2023-01-06 11:56:45 -0800

    hdfgre

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Print the name of the program
+ * @argc: number
+ * @argv: pointer array
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
