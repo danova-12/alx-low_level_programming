2ac13733348ae55743235a04cebe13457704baa8
dani <danielabesha7@gmail.com>
2023-01-10 09:01:52 -0800

    task1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * create_array - create an array of character
+ * @size: size of memory
+ * @c: character
+ *
+ * Return: pointer of chracter
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i = 0;
+	char *p = malloc(size);
+
+	if (size == 0 || p == NULL)
+		return (NULL);
+	else
+	{
+		while (i < size)
+		{
+			p[i] = c;
+			i++;
+		}
+	}
+	return (p);
+}
