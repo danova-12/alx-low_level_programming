982cb55e81450d95d7456d50e5b444d072014250
dani <danielabesha7@gmail.com>
2023-01-12 11:27:21 -0800

    hvheh

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,15 +9,15 @@
  */
 int *array_range(int min, int max)
 {
-	int *arr, i = 0, t = min;
+	int *array, i = 0, d = min;
 
 	if (min > max)
 		return (0);
-	arr = malloc((max - min + 1) * sizeof(int));
+	array = malloc((max - min + 1) * sizeof(int));
 
-	if (!arr)
+	if (array == NULL)
 		return (0);
 	while (i <= max - min)
-		arr[i++] = t++;
-	return (arr);
+		*(array + i) = d++;
+	return (array);
 }
