16c5711e4d2d8e03603fac65c654b3d3bc6845c3
dani <danielabesha7@gmail.com>
2023-01-12 09:01:29 -0800

    eydtyewdutewu

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * malloc_checked - A function that allocates memory using malloc.
+ * @b: size
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
