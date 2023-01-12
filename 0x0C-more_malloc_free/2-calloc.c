63cc0012c4ffad0d205a8e8e973662fa250fc55e
dani <danielabesha7@gmail.com>
2023-01-12 10:12:01 -0800

    ueueu

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,44 @@
+
+#include "main.h"
+/**
+ * setmemory - copy char
+ * @ptr: string
+ * @b: input
+ * @n: memory block size
+ * Return: pointer to memory allocated
+ */
+char *setmemory(char *ptr, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		ptr[i] = b;
+	}
+
+	return (ptr);
+}
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: n elements
+ * @size: bytes
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	_memset(p, 0, (nmemb * size));
+
+	return (p);
+}
