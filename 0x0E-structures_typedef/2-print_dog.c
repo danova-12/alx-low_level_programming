0e7c446a847910f443a2ef592f2f9f6e69cec011
dani <danielabesha7@gmail.com>
2023-01-16 10:24:31 -0800

    fgi3uif

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,19 +7,23 @@
  * Return: nothing
  */
 void print_dog(struct dog *d)
-{	
+{
 	if(d)
 	{
 
-		if (d->name == NULL || d->owner == NULL)
-		{
+		if (d->name == NULL)
+	
 			printf("Name: (nil)\n");
-		}
+
 		else
-		{
+
 			printf("Name: %s\n", d->name);
-			printf("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+
+		else
 			printf("Owner: %s\n", d->owner);
-		}
+		printf("Age: %f\n", d->age);
 	}
 }
