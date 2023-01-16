9a1ba03ffdb616a25e5bfb574ac312333c4bd3a8
dani <danielabesha7@gmail.com>
2023-01-16 09:39:49 -0800

    ugrui4u

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - intialize dog
+ * @d: pointer to object
+ * @name: name
+ * @age: age variable
+ * @owner: parameter
+ * Return: nothing
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
