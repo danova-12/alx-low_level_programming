6ffb4a49f3e4ea212e859a1643a5b0b671361928
dani <danielabesha7@gmail.com>
2023-01-16 09:20:23 -0800

    efrguifu

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,27 +1,3 @@
-#ifndef DOG_H
-#define DOG_H
-/**
- * struct dog - structure that defines dog property
- * @name: name of dog
- * @age: age of dog
- * @owner: owner name
- *
- */
-
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
 
 
 
-};
-
-
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif
