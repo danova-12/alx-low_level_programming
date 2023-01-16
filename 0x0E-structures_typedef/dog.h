4f4878c5225e9d299439e405bd2f6a69d7146e75
dani <danielabesha7@gmail.com>
2023-01-16 09:05:51 -0800

    first task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,25 @@
 #ifndef DOG
 #define DOG
+/**
+ * struct dog - structure that defines dog property
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner name
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+
+
+}
+
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
