d27310bc82d6236554cf4b402271df5c9c79230c
dani <danielabesha7@gmail.com>
2023-01-18 05:41:50 -0800

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,13 +7,10 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-	{
-		f(name);
-	
-	}
-	else
+	if (name == NULL || f == NULL)
 	{
 		return;
+	
 	}
+	f(name);
 }
