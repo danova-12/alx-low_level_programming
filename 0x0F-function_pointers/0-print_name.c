a25dba10ba7663944324d36a5da43110811f5ba9
dani <danielabesha7@gmail.com>
2023-01-18 05:30:08 -0800

    tsk0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - function thats prints name
+ * @name: char to display
+ * @f: A pointer function
+ * Return: nothing
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
