eff2c89af51ee5c987c2118c17250f3919f1211d
dani <danielabesha7@gmail.com>
2023-01-18 06:10:52 -0800

    gerfgieruif

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,27 @@
  * @f: A pointer function
  * Return: nothing
 */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f) /* if any is NULL */
 		return;
 
 	f(name);
+}
+/**
+ * print - eirii
+ * @namee: u3ueu3wswjsnn
+ * Return: 4f4r
+ */
+void print(char *namee)
+{
+    int i=0;
+    while(namee[i]!='\0')
+    {
+        _putchar(namee[i]);
+        i++;
+    }
+
+
 }
