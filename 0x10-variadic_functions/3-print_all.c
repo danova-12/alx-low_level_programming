86bacad5b74f820e4447364287265e604347755e
dani <danielabesha7@gmail.com>
2023-01-22 00:03:02 -0800

    cgrf

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,7 +12,7 @@ void print_all(const char * const format, ...)
 	const char t_arg[] = "cifs";
 
 	va_start(valist, format);
-	while (format[i] != '\0') 
+	while (format[i]) 
 	{
 		j = 0;
 		while (t_arg[j])
