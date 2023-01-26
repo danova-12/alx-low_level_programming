a75be95f69b6ac473c6be68011a627d96c892e64
dani <danielabesha7@gmail.com>
2023-01-26 10:38:15 -0800

    crfogyrei0yf

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,35 +9,19 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	size_t length = 0;
+	size_t a = 0;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
-	length = str_length(str);
+	while (str[a])
+		a++;
 
 	new->str = strdup(str);
-	new->len = length;
+	new->len = a;
 	new->next = *head;
 	*head = new;
 
 	return (*head);
 }
-/**
- * str_length - computes string length
- * @st: string pointer
- *
- * Return: length
- */
-size_t str_length(const char *st)
-{
-	size_t lent = 0;
-
-	while (st[lent] != '\0')
-	{
-		lent++;
-	}
-	return (lent);
-}
-
