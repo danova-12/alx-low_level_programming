1d6cf1fc0d31b6353e666e1cecd18a0a033f30c8
dani <danielabesha7@gmail.com>
2023-02-02 11:06:59 -0800

    ieiirifiir

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,7 +6,7 @@
   */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int first, place;
+	unsigned int uint, place;
 	int n;
 
 	if (!b)
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	for (n = 0; b[n]; n++)
 		;
 	n--;
-	for (place = 1, first = 0; n >= 0; n--)
+	for (place = 1, uint = 0; n >= 0; n--)
 	{
 		if (b[n] == '0')
 		{
@@ -24,11 +24,11 @@ unsigned int binary_to_uint(const char *b)
 		}
 		else if (b[n] == '1')
 		{
-		f	first += place;
+			uint += place;
 			place *= 2;
 			continue;
 		}
 		return (0);
 	}
-	return (first);
+	return (uint);
 }
