714445299c7c7b2a8a2b97ec1ef39aeed5d0350d
dani <danielabesha7@gmail.com>
2023-02-06 09:50:23 -0800

    yyff

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fild = 0, chk = 0, i = 0;
 
-	if (filename == NULL) 
+	if (filename == NULL)
 		return (-1);
 
 	fild = open(filename, O_WRONLY | O_APPEND);
